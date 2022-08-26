2fc4ce924b5ae0004141ef00cc689765d3540064
Anthony Delucchi <34591098+VinDEV99@users.noreply.github.com>
2022-08-25 18:06:15 -0700

    Unity Stuff
    All initial files of the Unity project

---
+++ Assets/Scripts/SelectableUnit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SelectableUnit : MonoBehaviour
+{
+    private NavMeshAgent Agent;
+    [SerializeField]
+    //private SpriteRenderer SelectionSprite;
+    private GameObject SelectionSprite;
+
+    private void Awake()
+    {
+        SelectionManager.Instance.AvailableUnits.Add(this);
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveTo(Vector3 Position)
+    {
+        Agent.SetDestination(Position);
+    }
+
+    //Add anything that needs to be done upon selection, like animation trigger or voice line for example
+    public void OnSelected()
+    {
+        SelectionSprite.gameObject.SetActive(true);
+    }
+
+    //same as above but on deselect
+    public void OnDeselected()
+    {
+        SelectionSprite.gameObject.SetActive(false);
+    }
+}
